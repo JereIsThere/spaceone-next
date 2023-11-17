e9325f753563b6202cb4b4d2c4f152d9afbc6925
jereisthere <fuumax2.0@gmail.com>
2023-11-17 17:43:46 +0100

    improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import LinkButton from "@/components/LinkButton";
+import { authOptions } from "./auth/nextauth"
+import { getServerSession } from "next-auth/next";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  const user = session?.user
+
   return (
     <>
+        <h1 style={{width: '100%', textAlign: 'center'}}>Welcome to the Homepage!</h1>
+        {
+          (!!user)
+          ?<h2 style={{width: '100%', textAlign: 'center'}}>You are logged in as {session?.user?.name}</h2>
+          : <LinkButton link="/login" text="Login!"/> 
+        }
         
     </>
   )
